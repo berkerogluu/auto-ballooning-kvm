031527ce723c4ea02ede68de69ccdbaa03d70fff
Berk Eroğlu <30663522+Scispow@users.noreply.github.com>
2019-09-24 00:39:11 +0300

    Add files via upload

---
+++ auto-balloon.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Script coded by Berk Eroglu on 09/23/2019
+
+#Variables that you can change MiB
+PROCESS_MEMORY_AT=10000      
+TOTAL_VM=60    
+MEMORY_PER_VM=2000          
+##############################
+
+AVAILABLE_MEM=$(awk '/^Mem/ {print $7}' <(free -m))
+
+if [[ $AVAILABLE_MEM -lt $PROCESS_MEMORY_AT ]]
+then
+   DEFLATE_MEMORY_PER_VM=$(($PROCESS_MEMORY_AT - $AVAILABLE_MEM))/$(($TOTAL_VM))
+   for i in `virsh list --all|awk '{print $2}'|grep -v Name`; do virsh setmem $i $(($MEMORY_PER_VM * 1000 - $DEFLATE_MEMORY_PER_VM * 1000)); done
+fi

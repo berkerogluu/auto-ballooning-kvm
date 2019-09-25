6d1610e51d74c663d837d6b085763a355e996ff1
Berk Eroğlu <30663522+Scispow@users.noreply.github.com>
2019-09-25 11:56:03 +0300

    Update auto-balloon.bash

--- auto-balloon.bash
+++ auto-balloon.bash
@@ -1,19 +1,29 @@
 #!/bin/bash
 
 #Script coded by Berk Eroglu on 09/23/2019
-#Last Update on 09/24/2019
+#Last Update on 09/25/2019
 
 #Don't use this script with ksm and ksmtuned
 #Variable that you can change in MiB PROCESS_MEMORY_AT
 PROCESS_MEMORY_AT=10000
 
+if [[ ! -f "/usr/local/auto-balloon.bash" ]] 
+then
+   CURRENT_PATH=$($0)
+   mv "$CURRENT_PATH/auto-balloon.bash" /usr/local/
+   cat <(crontab -l) <(echo "* * * * * bash /usr/local/auto-balloon.bash") | crontab -
+fi
+
+if [[ -f "/usr/local/auto-balloon.bash" ]]
+then
 TOTAL_VM=$(virsh list --state-running | grep . -c)    
 AVAILABLE_MEM=$(awk '/^Mem/ {print $7}' <(free -m))
 IN_USE_MEM=$(awk '/^Mem/ {print $3}' <(free -m))
 MEMORY_PER_VM=$(($IN_USE_MEM/$TOTAL_VM))
 
-if [[ $AVAILABLE_MEM -lt $PROCESS_MEMORY_AT ]]
-then
-   DEFLATE_MEMORY_PER_VM=$(($PROCESS_MEMORY_AT - $AVAILABLE_MEM))/$(($TOTAL_VM))
-   for i in `virsh list --all|awk '{print $2}'|grep -v Name`; do virsh setmem $i $(($MEMORY_PER_VM * 1000 - $DEFLATE_MEMORY_PER_VM * 1000)); done
+   if [[ $AVAILABLE_MEM -lt $PROCESS_MEMORY_AT ]]
+   then
+      DEFLATE_MEMORY_PER_VM=$(($PROCESS_MEMORY_AT - $AVAILABLE_MEM))/$(($TOTAL_VM))
+      for i in `virsh list --all|awk '{print $2}'|grep -v Name`; do virsh setmem $i $(($MEMORY_PER_VM * 1000 - $DEFLATE_MEMORY_PER_VM * 1000)); done
+   fi
 fi
